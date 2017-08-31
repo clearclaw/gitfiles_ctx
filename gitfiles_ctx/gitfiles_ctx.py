053ff4c5c6a25c5d8e0ee84dcd51ce3eb74cf37c
J C Lawrence <claw@kanga.nu>
2017-08-31 15:26:43 -0700

    SSH auth fix?

--- gitfiles_ctx/gitfiles_ctx.py
+++ gitfiles_ctx/gitfiles_ctx.py
@@ -1,20 +1,22 @@
 #! /usr/bin/env python
 
-import git, logtool, shutil, tempfile
+import git, logging, logtool, shutil, tempfile
 from path import Path
 
+LOG = logging.getLogger (__name__)
 # https://stackoverflow.com/questions/28291909/gitpython-and-ssh-keys
 # https://stackoverflow.com/questions/28803626/get-all-revisions-for-a-specific-file-in-gitpython
 
 class GitFiles_Ctx (object):
 
   @logtool.log_call
-  def __init__ (self, url, ssh_keyfile, prefix = "gitfiles_ctx__",
+  def __init__ (self, url, ssh_keypath, prefix = "gitfiles_ctx__",
                 branch = "master"):
     self.fpath = tempfile.mkdtemp (prefix = prefix)
-    ssh_cmd = "ssh -i {keyfile}".format (keyfile = ssh_keyfile)
-    with git.Git ().custom_environment (GIT_SSH_COMMAND = ssh_cmd):
-      self.repo = git.Repo.clone_from (url, self.fpath, branch = branch)
+    ssh_cmd = "ssh -i {keypath}".format (keypath = ssh_keypath)
+    git.Repo.clone_from (url, self.fpath,
+                         env = {'GIT_SSH_COMMAND': ssh_cmd},
+                         branch = branch)
 
   @logtool.log_call
   def __enter__ (self):
